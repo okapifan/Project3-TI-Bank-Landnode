da91084fafca2bffcfd66cbe2823393aeffc8019
okapifan <wxzegers@gmail.com>
2020-05-22 12:05:41 +0200

    Added getting destination from Json

--- src/App.java
+++ src/App.java
@@ -2,6 +2,9 @@
 public class App {
 
 	public static void main(String[] args) {
-		//
+		
+		//Bestemming zoeken uit binnenkomend JSON bestand uit variable jsonfile
+		String destination_country = jsonfile.getJSONObject("header").getString("receiveCountry");
+		String destination_bank = jsonfile.getJSONObject("header").getString("receiveBank");
 	}
 }
